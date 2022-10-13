a7336eeba6774dc02f562342eab03eac33853ec7
LordFeexz <anandafiqri1403@gmail.com>
2022-10-13 10:30:52 +0700

    rapihin

--- client/src/stores/music.js
+++ client/src/stores/music.js
@@ -40,107 +40,115 @@ export const useMusicStore = defineStore('music',{
     getters:{},
     actions:{
         play() {
-      if (this.audio.paused) {
-        this.audio.play();
-        this.isTimerPlaying = true;
-      } else {
-        this.audio.pause();
-        this.isTimerPlaying = false;
-      }
-    },
-    generateTime() {
-      let width = (100 / this.audio.duration) * this.audio.currentTime;
-      this.barWidth = width + "%";
-      this.circleLeft = width + "%";
-      let durmin = Math.floor(this.audio.duration / 60);
-      let dursec = Math.floor(this.audio.duration - durmin * 60);
-      let curmin = Math.floor(this.audio.currentTime / 60);
-      let cursec = Math.floor(this.audio.currentTime - curmin * 60);
-      if (durmin < 10) {
-        durmin = "0" + durmin;
-      }
-      if (dursec < 10) {
-        dursec = "0" + dursec;
-      }
-      if (curmin < 10) {
-        curmin = "0" + curmin;
-      }
-      if (cursec < 10) {
-        cursec = "0" + cursec;
-      }
-      this.duration = durmin + ":" + dursec;
-      this.currentTime = curmin + ":" + cursec;
-    },
-    updateBar(x) {
-      let progress = this.$refs.progress;
-      let maxduration = this.audio.duration;
-      let position = x - progress.offsetLeft;
-      let percentage = (100 * position) / progress.offsetWidth;
-      if (percentage > 100) {
-        percentage = 100;
-      }
-      if (percentage < 0) {
-        percentage = 0;
-      }
-      this.barWidth = percentage + "%";
-      this.circleLeft = percentage + "%";
-      this.audio.currentTime = (maxduration * percentage) / 100;
-      this.audio.play();
-    },
-    clickProgress(e) {
-      this.isTimerPlaying = true;
-      this.audio.pause();
-      this.updateBar(e.pageX);
-    },
-    prevTrack() {
-      this.transitionName = "scale-in";
-      this.isShowCover = false;
-      if (this.currentTrackIndex > 0) {
-        this.currentTrackIndex--;
-      } else {
-        this.currentTrackIndex = this.tracks.length - 1;
-      }
-      this.currentTrack = this.tracks[this.currentTrackIndex];
-      this.resetPlayer();
-    },
-    nextTrack() {
-      this.transitionName = "scale-out";
-      this.isShowCover = false;
-      if (this.currentTrackIndex < this.tracks.length - 1) {
-        this.currentTrackIndex++;
-      } else {
-        this.currentTrackIndex = 0;
-      }
-      this.currentTrack = this.tracks[this.currentTrackIndex];
-      this.resetPlayer();
-    },
-    resetPlayer() {
-      this.barWidth = 0;
-      this.circleLeft = 0;
-      this.audio.currentTime = 0;
-      this.audio.src = this.currentTrack.source;
-      setTimeout(() => {
-        if (this.isTimerPlaying) {
+          if (this.audio.paused) {
+            this.audio.play();
+            this.isTimerPlaying = true;
+          } 
+          else {
+            this.audio.pause();
+            this.isTimerPlaying = false;
+          }
+        },
+        generateTime() {
+          let width = (100 / this.audio.duration) * this.audio.currentTime;
+            this.barWidth = width + "%";
+            this.circleLeft = width + "%";
+            let durmin = Math.floor(this.audio.duration / 60);
+            let dursec = Math.floor(this.audio.duration - durmin * 60);
+            let curmin = Math.floor(this.audio.currentTime / 60);
+            let cursec = Math.floor(this.audio.currentTime - curmin * 60);
+          if (durmin < 10) {
+            durmin = "0" + durmin;
+          }
+          if (dursec < 10) {
+            dursec = "0" + dursec;
+          }
+          if (curmin < 10) {
+            curmin = "0" + curmin;
+          }
+          if (cursec < 10) {
+            cursec = "0" + cursec;
+          }
+            this.duration = durmin + ":" + dursec;
+            this.currentTime = curmin + ":" + cursec;
+          },
+        updateBar(el) {
+          let progress = this.$refs.progress;
+          let maxduration = this.audio.duration;
+          let position = el - progress.offsetLeft;
+          let percentage = (100 * position) / progress.offsetWidth;
+          if (percentage > 100) {
+            percentage = 100;
+          }
+          if (percentage < 0) {
+            percentage = 0;
+          }
+          this.barWidth = percentage + "%";
+          this.circleLeft = percentage + "%";
+          this.audio.currentTime = (maxduration * percentage) / 100;
           this.audio.play();
-        } else {
+        },
+        clickProgress(el) {
+          this.isTimerPlaying = true;
           this.audio.pause();
-        }
-      }, 300);
-    },
-    favorite() {
-      this.tracks[this.currentTrackIndex].favorited = !this.tracks[
-        this.currentTrackIndex
-      ].favorited;
-    },
-    async fetchMusic(){
-      try {
-        const { data } = await axios({
-          method:'get',
-          url:`${this.url}/music`
-        })
-        this.tracks = data
-      } catch (err) {
-        console.log(err)
+          this.updateBar(el.pageX);
+        },
+        prevTrack() {
+          this.transitionName = "scale-in";
+          this.isShowCover = false;
+          if (this.currentTrackIndex > 0) {
+            this.currentTrackIndex--;
+          }
+          else {
+            this.currentTrackIndex = this.tracks.length - 1;
+          }
+          this.currentTrack = this.tracks[this.currentTrackIndex];
+          this.resetPlayer();
+        },
+        nextTrack() {
+          this.transitionName = "scale-out";
+          this.isShowCover = false;
+          if (this.currentTrackIndex < this.tracks.length - 1) {
+            this.currentTrackIndex++;
+          } 
+          else {
+            this.currentTrackIndex = 0;
+          }
+          this.currentTrack = this.tracks[this.currentTrackIndex];
+          this.resetPlayer();
+        },
+        resetPlayer() {
+          this.barWidth = 0;
+          this.circleLeft = 0;
+          this.audio.currentTime = 0;
+          this.audio.src = this.currentTrack.source;
+          setTimeout(() => {
+            if (this.isTimerPlaying) {
+              this.audio.play();
+            } 
+            else {
+              this.audio.pause();
+            }
+          }, 300);
+        },
+        favorite() {
+          this.tracks[this.currentTrackIndex].favorited = !this.tracks[
+          this.currentTrackIndex
+          ].favorited;
+        },
+        async fetchMusic(){
+          try {
+            const { data } = await axios({
+              method:'get',
+              url:`${this.url}/music`
+            })
+            this.tracks = data
+          } catch (err) {
+          Swal.fire({
+          icon:"error",
+          title:"Oops...",
+          text:`${err.response.data.message}`
+          })
       }
     }
   }
